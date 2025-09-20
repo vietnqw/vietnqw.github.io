0060bae5becb24ca114af63052c159fd2524e4e9
Viet Ngo Quang <vietnq.work@gmail.com>
2025-09-20 11:52:24 +0700

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Space_Grotesk, Inter, JetBrains_Mono } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Suspense } from "react"
 import "./globals.css"
+import Favicon from "/public/favicon.ico"
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -32,7 +33,7 @@ export const metadata: Metadata = {
     "Personal portfolio of Viet - Software Engineer, Tech Explorer, and Creator. Showcasing projects developed by me and my team.",
   generator: "v0.app",
   icons: {
-    icon: "/favicon.ico",
+    icon: Favicon.src,
   },
 }
 
@@ -43,9 +44,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
+      <head />
       <body className={`${spaceGrotesk.variable} ${inter.variable} ${jetbrainsMono.variable} font-inter antialiased`}>
         <Suspense fallback={null}>
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
